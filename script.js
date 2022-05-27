dc0126a203755920b04188f1f14345441e201145
jgomez002 <jdg8858@nyu.edu>
2022-05-27 04:48:33 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function validate(){
+  var password= document.getElementById("password").value;
+  {
 
+    if (password == "perspective") {
+      alert("slayyyy");
+      window.location.href = "desktop.html";
+    }
+    else {
+      alert("deslay sad");
+    }
+  }
+}
+
+var input = document.getElementById("password");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("login").click();
+  }
+});
