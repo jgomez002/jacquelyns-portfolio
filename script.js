cf05fc53ad5b259516523112eeac7b8924bd936d
jgomez002 <jdg8858@nyu.edu>
2022-06-06 04:55:19 -0400

    update

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+// let loginbutton = document.querySelector("#login");
+// loginbutton.addEventListener("click", shake(), false);
+
+function shake(event){
+  document.getElementById("loginsq").style.animation = "none";
+  requestAnimationFrame(() => {
+    document.getElementById("loginsq").style.animation = "wrong 0.5s";
+  });
+}
+
+
 function validate(){
   var password= document.getElementById("password").value;
-  {
 
+  {
     if (password == "perspective") {
       // alert("slayyyy");
       window.location.href = "desktop.html";
     }
     else {
       // alert("deslay sad");
+      shake()
+        // document.getElementById("loginsq").style.animation = "wrong 0.5s"
+      }
+
+
     }
   }
-}
+
 
 var input = document.getElementById("password");
 input.addEventListener("keypress", function(event) {
